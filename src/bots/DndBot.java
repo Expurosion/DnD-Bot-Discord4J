270b8e9f25b1452f7ce25f302a9ebad390e01c30
Expurosion <yf/JSEvp'c@.5mgJx"lIs6|3P>
2017-05-16 19:34:20 -0500

    Commented out example

--- src/bots/DndBot.java
+++ src/bots/DndBot.java
@@ -7,6 +7,8 @@ import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.api.events.IListener;
 import sx.blah.discord.handle.impl.events.guild.channel.message.MessageReceivedEvent;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
 
 public class DndBot extends BaseBot implements IListener<MessageReceivedEvent> {
 
@@ -27,7 +29,29 @@ public class DndBot extends BaseBot implements IListener<MessageReceivedEvent> {
 	public void handle(MessageReceivedEvent event)
 	{
 		// TODO Auto-generated method stub
-
+		IMessage message = event.getMessage(); // Gets the message from the event object NOTE: This is not the content of the message, but the object itself
+		IChannel channel = message.getChannel(); // Gets the channel in which this message was sent.
+		// try
+		// {
+		// // Builds (sends) and new message in the channel that the original message was sent with the content of the original message.
+		// new MessageBuilder(this.client).withChannel(channel).withContent(message.getContent())
+		// .build();
+		// }
+		// catch (RateLimitException e)
+		// { // RateLimitException thrown. The bot is sending messages too quickly!
+		// System.err.print("Sending messages too quickly!");
+		// e.printStackTrace();
+		// }
+		// catch (DiscordException e)
+		// { // DiscordException thrown. Many possibilities. Use getErrorMessage() to see what went wrong.
+		// System.err.print(e.getErrorMessage()); // Print the error message sent by Discord
+		// e.printStackTrace();
+		// }
+		// catch (MissingPermissionsException e)
+		// { // MissingPermissionsException thrown. The bot doesn't have permission to send the message!
+		// System.err.print("Missing permissions for channel!");
+		// e.printStackTrace();
+		// }
 	}
 
 }
